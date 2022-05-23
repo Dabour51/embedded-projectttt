900e39692d76c78cb8b4de7a87a524016a298807
Mohameddmostafaaa <105743479+Mohameddmostafaaa@users.noreply.github.com>
2022-05-23 14:33:37 +0200

    Update and rename Lcd to Lcd.c

--- Lcd.c
+++ Lcd.c
@@ -1,7 +1,7 @@
 void lcd_data(unsigned char data)
 	{
 		GPIO_PORTA_DATA_R = 0x20 ;
-    GPIO_PORTA_DATA_R |= 0x80; // e=1 (pulse) to secure data		
+    GPIO_PORTA_DATA_R |= 0x80; 		
 	  GPIO_PORTB_DATA_R = data;
 	  
 	  delay_micro(10);

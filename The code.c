d520dca7c0cd31e5a9304df967b4a1a1f83d518e
Mohameddmostafaaa <105743479+Mohameddmostafaaa@users.noreply.github.com>
2022-05-25 16:35:08 +0200

    Update The code.c

--- The code.c
+++ The code.c
@@ -96,3 +96,82 @@ void LCD_String (char *str)
  lcd_4bits_data(str[i]); /* Call LCD data write */
  }
 }
+void keypad_init ()
+{
+ SYSCTL_RCGCGPIO_R |= 0x4;
+ while((SYSCTL_PRGPIO_R &= 0x4)==0) ; 
+ SYSCTL_RCGCGPIO_R |= 0x10;
+ while((SYSCTL_PRGPIO_R &= 0x10)==0) ; 
+ GPIO_PORTE_DIR_R |=0xf;
+ GPIO_PORTE_DEN_R |=0xf;
+ GPIO_PORTE_ODR_R |=0XF;
+ GPIO_PORTC_DIR_R &=~ 0xf0;
+ GPIO_PORTC_DEN_R |= 0xf0;
+ GPIO_PORTC_PUR_R |= 0xf0;
+}
+unsigned char get_key ()
+{
+ const unsigned char keypad [4][4]={ {'1','2','3','A'},
+ 
+{'4','5','6','B'},
+ 
+{'7','8','9','C'},
+{'*','0','#','D'} };
+unsigned int row , col ;
+ GPIO_PORTE_DATA_R &=~ 0xf;
+ col = GPIO_PORTC_DATA_R & 0xf0;
+ if (col == 0xf0)
+ return 0 ;
+ while(1)
+ {
+ row=0 ;
+ GPIO_PORTE_DATA_R = 0xe;
+ delay_micro(10);
+ col = GPIO_PORTC_DATA_R &0xf0;
+ if(col != 0xf0)
+ break;
+ row=1 ;
+ GPIO_PORTE_DATA_R = 0xd;
+ delay_micro(10);
+ col = GPIO_PORTC_DATA_R &0xf0;
+ if(col != 0xf0)
+ break;
+ row=2 ;
+ GPIO_PORTE_DATA_R = 0xb;
+ delay_micro(10);
+ col = GPIO_PORTC_DATA_R &0xf0;
+ if(col != 0xf0)
+ break;
+ row=3 ;
+ GPIO_PORTE_DATA_R = 0x7;
+ delay_micro(10);
+ col = GPIO_PORTC_DATA_R &0xf0;
+ if(col != 0xf0)
+ break;
+ return 0;
+ }
+ if(col == 0xe0)
+ return keypad[row][0];
+ if(col == 0x0d0)
+ return keypad[row][1];
+ if(col == 0x0b0)
+ return keypad[row][2];
+ if(col == 0x070)
+ return keypad[row][3];
+ return 0 ;
+}
+void Buzzer_Init(){ //using port D7
+ SYSCTL_RCGCGPIO_R |= 0x08;
+ while ((SYSCTL_PRGPIO_R & 0x08) == 0);
+ GPIO_PORTD_LOCK_R = GPIO_LOCK_KEY;
+ GPIO_PORTD_CR_R |= 0x80;
+ GPIO_PORTD_AFSEL_R &= ~0x80;
+ GPIO_PORTD_PCTL_R &= ~0xF0000000;
+ GPIO_PORTD_AMSEL_R &= ~0x80;
+ GPIO_PORTD_DEN_R |= 0x80;
+ GPIO_PORTD_DIR_R |= 0x80;
+ GPIO_PORTD_DATA_R &= ~0x80;
+}
+void Buzzer_sound(){
+GPIO_PORTD_DATA_R =GPIO_PORTD_DATA_R ^ 0x80;// buzzer on or off
+}

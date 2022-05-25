f555efbd71bb0f8004446e68a4adc870406a3c37
Ziadyasser59 <105709906+Ziadyasser59@users.noreply.github.com>
2022-05-25 16:44:48 +0200

    Update The code.c

--- The code.c
+++ The code.c
@@ -376,3 +376,111 @@ time = weight * 50;
  break;
 }
  }
+void chicken_cooking(){
+char value,weight;
+int time,sw12;
+LCD_String("Chicken Weight? (1 to 9)");
+while(1){
+do{
+value = get_key();
+}while(value==0);
+lcd_4bits_cmd(0x1);
+lcd_4bits_cmd(0x80);
+if (value > '0' & value <= '9'){
+lcd_4bits_data(value);
+delay(500);
+weight = value;
+}
+else if((value <= '0') | (value > '9')){
+lcd_4bits_cmd(0x1);
+delay(100);
+LCD_String(" Err");
+delay(2000);
+lcd_4bits_cmd(0x1);
+chicken_cooking();
+}
+do{
+sw12 = sw12_input();
+}while((sw12 == 0x11));
+if ((sw12 & 0x10) == 0x00 ){
+lcd_4bits_cmd(0x1);
+lcd_4bits_cmd(0x80);
+chicken_cooking() ;
+}
+else if ((sw12 & 0x01) == 0x00){
+lcd_4bits_cmd(0x1);
+delay(100);
+weight = weight -48;
+if((weight >0) && (weight <5)){
+time = weight * 12;
+timer(time);
+}
+else if ((weight >4) && (weight <10)){
+time = (weight * 12) + 40;
+timer(time);
+}
+}
+}
+}
+void other_cooking(){
+int i,sw12;
+char value;
+char time[30];
+char cmds[4] ={0xc4,0xc3,0xc1,0xc0};
+int countdown;
+L3: LCD_String("Cooking time? ");
+lcd_4bits_cmd(0xC0);
+LCD_String("00:00");
+while(1){
+for(i=0;i<4;i++){
+do{
+delay(200);
+value = get_key();
+sw12 = sw12_input();
+}while(value==0 & (sw12 == 0x11));
+if ((value >='0') && (value <= '9')){
+lcd_4bits_cmd(cmds[0]);
+lcd_4bits_data(value);
+time[i] = value;
+if(i==1){
+lcd_4bits_cmd(cmds[i]);
+lcd_4bits_data(time[i-1]);
+}
+if(i==2){
+lcd_4bits_cmd(cmds[i]);
+lcd_4bits_data(time[i-2]);
+lcd_4bits_cmd(cmds[i-1]);
+lcd_4bits_data(time[i-1]);
+}
+if(i==3){
+lcd_4bits_cmd(cmds[i]);
+lcd_4bits_data(time[i-3]);
+lcd_4bits_cmd(cmds[i-1]);
+lcd_4bits_data(time[i-2]);
+lcd_4bits_cmd(cmds[i-2]);
+lcd_4bits_data(time[i-1]);
+}
+}
+else if ((sw12 & 0x10) == 0x00 ){
+lcd_4bits_cmd(0x1);
+lcd_4bits_cmd(0x80);
+goto L3;
+}
+else if ((sw12 & 0x01) == 0x00){
+lcd_4bits_cmd(0x1);
+delay(100);
+countdown = atoi(time);
+timer(countdown);
+return;
+}
+else if((value < '0') | (value > '9')){
+lcd_4bits_cmd(0x1);
+delay(100);
+LCD_String(" Err");
+delay(2000);
+lcd_4bits_cmd(0x1);
+goto L3;
+}
+}
+}
+}
